2bca0559cfe8b73f7e6cc58c0dcc8ab8c451884e
Your Name <you@example.com>
2018-04-23 17:14:46 +0100

    Part 2 complete

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-for (let number = 1; number < 101; number++) {
-    const FizzBuzz = "FizzBuzz";
-    const Buzz = "Buzz";
-    const Fizz = "Fizz";
-    const Bang = "Bang";
-    const FizzBang = "FizzBang";
-    const BuzzBang = "BuzzBang";
-    const Bong = "Bong";
+const Buzz = "Buzz";
+const Fizz = "Fizz";
+const Bang = "Bang";
+const Bong = "Bong";
+const Fezz = "Fezz";
+
+
+for (let number = 1; number < 301; number++) {
+    let message = [];
     if (numberIsBong(number)) {
-        console.log(Bong)
-    } else if (numberIsBuzzBang(number)) {
-        console.log(BuzzBang);
-    } else if (numberIsFizzBang(number)) {
-        console.log(FizzBang);
-    } else if (numberIsFizzBuzz(number)) {
-        console.log(FizzBuzz);
-    } else if (numberIsBang(number)) {
-        console.log(Bang)
-    } else if (numberIsBuzz(number)) {
-        console.log(Buzz);
-    } else if (numberIsFizz(number)) {
-        console.log(Fizz);
+        if (numberIsFezz(number)) {
+            message.push(Fezz);
+        }
+        message.push(Bong);
     } else {
+        if (numberIsFizz(number)) {
+            message.push(Fizz);
+        }
+        if (numberIsBuzz(number)) {
+            message.push(Buzz);
+        }
+        if (numberIsBang(number)) {
+            message.push(Bang);
+        }
+        if (numberIsFezz(number)) {
+            if (message.length > 0) {
+                let firstLetter = message[0].charAt(0).toUpperCase();
+                if (firstLetter == "B") {
+                    message.unshift(Fezz);
+                }
+            } else {
+                message.push(Fezz);
+            }
+        }
+    }
+    if (number % 17 === 0) {
+        message.reverse();
+    }
+    if (message.length == 0) {
         console.log(number);
+    } else {
+        console.log(message.join(""));
     }
 }
 
@@ -29,30 +47,22 @@ function numberIsDivisible(number, divisor) {
     return number % divisor == 0;
 }
 
-function numberIsFizzBuzz(number) {
-    return numberIsDivisible(number, 5) && numberIsDivisible(number, 3);
+function numberIsFizz(number) {
+    return numberIsDivisible(number, 3);
 }
 
 function numberIsBuzz(number) {
     return numberIsDivisible(number, 5);
 }
 
-function numberIsFizz(number) {
-    return numberIsDivisible(number, 3);
-}
-
 function numberIsBang(number) {
     return numberIsDivisible(number, 7);
 }
 
-function numberIsFizzBang(number) {
-    return (numberIsBang(number) && numberIsFizz(number));
-}
-
-function numberIsBuzzBang(number) {
-    return (numberIsBang(number) && numberIsBuzz(number));
-}
-
 function numberIsBong(number) {
     return numberIsDivisible(number, 11);
+}
+
+function numberIsFezz(number) {
+    return numberIsDivisible(number, 13);
 }
