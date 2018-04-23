b332b2dc9f2e4fb889686b81682662fd54778575
Your Name <you@example.com>
2018-04-23 12:55:50 +0100

    now 'bangs' on multiples of 7

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ for (let number = 1; number < 101; number++) {
     const FizzBuzz = "FizzBuzz";
     const Buzz = "Buzz";
     const Fizz = "Fizz";
+    const Bang = "Bang";
     if (numberIsFizzBuzz(number)) {
         console.log(FizzBuzz);
+    } else if (numberIsBang(number)) {
+        console.log(Bang)
     } else if (numberIsBuzz(number)) {
         console.log(Buzz);
     } else if (numberIsFizz(number)) {
@@ -13,19 +16,24 @@ for (let number = 1; number < 101; number++) {
     }
 }
 
-
-function numberIsDivisibleByDivisor(number, divisor) {
+function numberIsDivisible(number, divisor) {
     return number % divisor == 0;
 }
 
 function numberIsFizzBuzz(number) {
-    return numberIsDivisibleByDivisor(number, 5) && numberIsDivisibleByDivisor(number, 3);
+    return numberIsDivisible(number, 5) && numberIsDivisible(number, 3);
 }
 
 function numberIsBuzz(number) {
-    return numberIsDivisibleByDivisor(number, 5)
+    return numberIsDivisible(number, 5);
 }
 
 function numberIsFizz(number) {
-    return numberIsDivisibleByDivisor(number, 3)
+    return numberIsDivisible(number, 3);
 }
+
+function numberIsBang(number) {
+    return numberIsDivisible(number, 7);
+}
+
+// If a number is a multiple of 7, print "Bang" instead of the number. For numbers which are multiples of seven and three / five, append Bang to what you'd have printed anyway. (E.g. 3 * 7 = 21: "FizzBang").
