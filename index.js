c035c0f60e9a9c3ffb0b30d3856a2ea938cb0d20
Your Name <you@example.com>
2018-04-23 12:28:14 +0100

    Refactored number comparison functions

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 for (let i = 1; i < 101; i++) {
     const number = i;
-    if (numberIsDivisibleByDivisor(number, 5) && numberIsDivisibleByDivisor(number, 3)) {
+    if (numberIsFizzBuzz(number)) {
         console.log("FizzBuzz");
-    } else if (numberIsDivisibleByDivisor(number, 5)) {
+    } else if (numberIsBuzz(number)) {
         console.log("Buzz");
-    } else if (numberIsDivisibleByDivisor(number, 3)) {
+    } else if (numberIsFizz(number)) {
         console.log("Fizz");
     } else {
         console.log(number);
     }
 }
 
+
 function numberIsDivisibleByDivisor(number, divisor) {
     if (number % divisor === 0) {
+        return true;
+    }
+}
+
+function numberIsFizzBuzz(number) {
+    if (numberIsDivisibleByDivisor(number, 5) && numberIsDivisibleByDivisor(number, 3)) {
+        return true;
+    }
+}
+
+function numberIsBuzz(number) {
+    if (numberIsDivisibleByDivisor(number, 5)) {
+        return true;
+    }
+}
+
+function numberIsFizz(number) {
+    if (numberIsDivisibleByDivisor(number, 3)) {
         return true;
     }
 }
